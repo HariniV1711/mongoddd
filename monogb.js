994ea7781228695eb7d6fd2c6104a8ef9030e913
HariniV1711 <117363518+HariniV1711@users.noreply.github.com>
2023-01-12 20:01:12 +0530

    Add files via upload

---
+++ monogb.js
@@ -0,0 +1,87 @@
+/*_id
+63c014291bb90f2495917c2a
+name
+"Coyote Hills Golf Course"
+noofslot
+"30"
+pertee
+"200"
+price
+"3000"
+date
+"2.3.2023"
+time
+"08.00am-02.00pm"
+description
+"very nice"
+_id
+63c014291bb90f2495917c2b
+name
+"American Classic Golf Club"
+noofslot
+"20"
+pertee
+"100"
+price
+"1000"
+date
+"3.3.2023"
+time
+"09.00am-11.00pm"
+description
+"good"
+_id
+63c014291bb90f2495917c2c
+name
+"Monarch Bay Golf Club"
+noofslot
+"15"
+pertee
+"150"
+price
+"1500"
+date
+"4.3.2023"
+time
+"10.00am-10.00pm"
+description
+"bad"
+_id
+63c014291bb90f2495917c2d
+name
+"South Shore Golf Course"
+noofslot
+"40"
+pertee
+"400"
+price
+"4000"
+date
+"5.3.2023"
+time
+"11.00am-12.00pm"
+description
+"very good"
+
+
+
+
+
+_id
+63c014291bb90f2495917c2e
+name
+"American Golf Club-Vero Beach"
+noofslot
+"25"
+pertee
+"250"
+price
+"2500"
+date
+"6.3.2023"
+time
+"12.00am-12pm"
+description
+"intermediate"
+
+*/
